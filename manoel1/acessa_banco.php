e53c433a91d9dec14bbd406afca3d84b6dbdc839
Alisson Oliveira Ferreira <linukiss@gmail.com>
2015-08-04 15:48:12 -0300

    configurado e testadp db para dev

--- manoel1/acessa_banco.php
+++ manoel1/acessa_banco.php
@@ -1,5 +1,5 @@
 <?php
-     $conexao=mysql_connect ("localhost","root","")
+     $conexao=mysql_connect ("localhost","root","d3v0p5")
       or die ("error ao conectar");
      $select = mysql_select_db ("espirita")
     or die ("error ao selecionar");
